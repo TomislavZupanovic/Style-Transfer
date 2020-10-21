df151bf3c9c24116f84b9d14591d1ff31f1191cb
TomislavZupanovic <tzupan01@fesb.hr>
2020-10-21 12:00:03 +0200

    Added painter class

---
+++ src/model.py
@@ -0,0 +1,33 @@
+import torch
+import torch.optim as optim
+from torchvision import models
+
+
+class Painter(object):
+    def __init__(self):
+        self.model = models.vgg16(pretrained=True).features
+        self.layers = None
+        for parameter in self.model.parameters():
+            parameter.requires_grad = False
+
+    def get_features(self, image):
+        if self.layers is None:
+            self.layers = {'0': 'conv1_1',
+                           '5': 'conv2_1',
+                           '10': 'conv3_1',
+                           '19': 'conv4_1',
+                           '21': 'conv4_2',
+                           '28': 'conv5_1'}
+        features = []
+        x = image
+        for name, layer in self.model._modules.items():
+            x = layer(x)
+            if name in self.layers:
+                features[self.layers[name]] = x
+        return features
+
+    @staticmethod
+    def gram_matrix(tensor):
+        _, d, h, w = tensor.size()
+        tensor = tensor.view(d, h*w)
+        return torch.mm(tensor, tensor.t())
